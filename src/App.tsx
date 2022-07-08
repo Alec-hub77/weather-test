9b8055f5b62f752491c5959cfdcf8735f1456c50
Oleg Ostapiuk <alec.ost@Olegs-MacBook-Pro.local>
2022-07-08 12:42:27 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,7 @@ import { weatherStore } from "./store/store";
 import { observer } from "mobx-react-lite";
 import Header from "./components/header/Header";
 import SearchInput from "./components/input/SearchInput";
-import WeatherCard from "./components/weather-card/WeatherCard";
-import { useEffect } from "react";
 import Home from "./pages/Home";
-
-// const citiesList = [
-//   {
-//     city: "London",
-//     isSaved: true
-//   },
-//   {
-//     city: "Paris",
-//     isSaved: true
-//   }
-// ];
 
 const App: React.FC = () => {
   return (
